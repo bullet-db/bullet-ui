c2825c080e31a945f654633dfff6632829525e1f
Akshai Sarma <asarma@yahoo-inc.com>
2020-05-12 18:16:08 -0700

    Adding a native class paginated table and converting schema-table to glimmer

---
+++ app/components/paginated-table.js
@@ -0,0 +1,107 @@
+/*
+ *  Copyright 2020, Yahoo Inc.
+ *  Licensed under the terms of the Apache License, Version 2.0.
+ *  See the LICENSE file associated with the project for terms.
+ */
+import { typeOf } from '@ember/utils';
+import { action, computed, get } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class PaginatedTable extends Component {
+  @tracked pageSize = 15;
+  @tracked firstNewRow = 0;
+  @tracked sortColumn = null;
+  extractors = null;
+  useDefaultStringExtractor = true;
+  appendMode = false;
+
+  @computed('rows.[]')
+  get numberOfRows() {
+    return this.rows.length;
+  }
+
+  get haveMoreRows() {
+    return this.firstNewRow < this.numberOfRows;
+  }
+
+  addRows(start, end) {
+    end = Math.min(this.numberOfRows, end);
+    let toAdd = this.rows.slice(start, end);
+    this.firstNewRow = end;
+    this.table.addRows(toAdd);
+  }
+
+  addPages(pages = 1) {
+    let { firstNewRow, pageSize } = this;
+    let lastNewRow = firstNewRow + (pageSize * pages);
+    return this.addRows(firstNewRow, lastNewRow);
+  }
+
+  defaultExtractor(column) {
+    return row => {
+      // Use regular instead of get in case column has '.' Only doing it here because the defaultExtractor is used
+      // for unknown column names. When we define columns, we should not add columns with '.' in the name
+      let value = row[column];
+      let valueType = typeOf(value);
+      if (valueType === 'string' || valueType === 'number' || valueType === 'boolean' || valueType === 'date') {
+        return value;
+      }
+      // If not a primitive, return a String version
+      return String(value);
+    };
+  }
+
+  getExtractor(column) {
+    let extractor = get(this, `extractors.${column}`);
+    if (extractor) {
+      return extractor;
+    }
+    // Default String convertor
+    if (this.useDefaultStringExtractor) {
+      return a => String(a.get(column));
+    }
+    // Identity
+    return this.defaultExtractor(column);
+  }
+
+  sortBy(column, direction = 'ascending') {
+    let rows = this.rows;
+    rows.sort((a, b) => {
+      let extractor = this.getExtractor(column);
+      let itemA = extractor(a);
+      let itemB = extractor(b);
+      let comparison = 0;
+      if (itemA < itemB) {
+        comparison = -1;
+      } else if (itemA > itemB) {
+        comparison = 1;
+      }
+      return direction === 'ascending' ? comparison : -comparison;
+    });
+  }
+
+  reset(forceReset = false) {
+    let table = this.table;
+    // Reset table if not in appendMode
+    if (!this.appendMode || forceReset) {
+      table.setRows([]);
+      this.firstNewRow = 0;
+    }
+  }
+
+  @action
+  onColumnClick(column) {
+    this.sortColumn = column;
+    this.reset(true);
+    this.sortBy(column.valuePath, column.ascending ? 'ascending' : 'descending');
+    this.addPages();
+  }
+
+  @action
+  onScrolledToBottom() {
+    if (!this.isDestroyed && !this.isDestroying && this.haveMoreRows) {
+      this.addPages();
+    }
+  }
+}
--- app/components/schema-table.js
+++ app/components/schema-table.js
@@ -4,49 +4,39 @@
  *  See the LICENSE file associated with the project for terms.
  */
 import { A } from '@ember/array';
+import { action, computed } from '@ember/object';
 import { typeOf } from '@ember/utils';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
+import Component from '@glimmer/component';
 import Table from 'ember-light-table';
-import PaginatedTable from 'bullet-ui/mixins/paginated-table';
+import PaginatedTable from 'bullet-ui/components/paginated-table';
 
-export default Component.extend(PaginatedTable, {
-  classNames: ['schema-table'],
-  classNameBindings: ['isNested'],
-  queries: null,
-  pageSize: 10,
-  /**
-   * If set to true, the table behaves as a nested table without headers and will not expand further. Meant to
-   * be used when the table is nested within itself.
-   *
-   * @type {Boolean}
-   */
-  isNested: false,
-
-  rows: computed('fields.[]', function() {
-    let fields = this.fields;
-    // This needs to handle arrays (for enumeratedColumns) and the model result collection
-    return typeOf(fields) === 'array' ? fields : fields.toArray();
-  }),
-
-  columns: A([
+export default class SchemaTableComponent extends PaginatedTable {
+  columns = A([
     { label: 'Field', width: '250px', valuePath: 'name', cellComponent: 'cells/schema-name-entry' },
     { label: 'Type', width: '250px', valuePath: 'qualifiedType', cellComponent: 'cells/schema-type-entry' },
     { label: 'Description', valuePath: 'description', cellComponent: 'cells/schema-description-entry' }
-  ]),
+  ]);
 
-  init() {
-    this._super(...arguments);
-    this.set('table', Table.create({ columns: this.columns }));
+  @computed('fields.[]')
+  get rows() {
+    let fields = this.args.fields;
+    // This needs to handle arrays (for enumeratedColumns) and the model result collection
+    return typeOf(fields) === 'array' ? fields : fields.toArray();
+  }
+
+  constructor() {
+    super(...arguments);
+    this.pageSize = 10;
+    this.table = Table.create({ columns: this.columns });
     this.sortBy('name', 'ascending');
     this.addPages(2);
-  },
+  }
 
-  actions: {
-    onRowClick(row) {
-      if (!row.get('hasEnumerations')) {
-        row.set('expanded', false);
-      }
+  @action
+  onRowClick(row) {
+    if (!row.get('hasEnumerations')) {
+      row.set('expanded', false);
     }
   }
-});
+}
