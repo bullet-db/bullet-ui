7bd69c6e6d4eb05e6257a39415d71a4d24467521
Akshai Sarma <asarma@yahoo-inc.com>
2020-05-29 16:10:52 -0700

    Migrating labeled-input

--- app/components/labeled-input.js
+++ app/components/labeled-input.js
@@ -3,14 +3,27 @@
  *  Licensed under the terms of the Apache License, Version 2.0.
  *  See the LICENSE file associated with the project for terms.
  */
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { guidFor } from '@ember/object/internals';
 
-export default Component.extend({
-  classNames: ['labeled-input'],
-  fieldName: null,
-  fieldValue: null,
-  type: 'text',
-  placeholder: null,
-  disabled: false,
-  maxlength: 30
-});
+export default class LabeledInputComponent extends Component {
+  id;
+
+  constructor() {
+    super(...arguments);
+    this.id = `${guidFor(this)}-${this.args.fieldName}`;
+  }
+
+  get type() {
+    return this.args.type || 'text';
+  }
+
+  get disabled() {
+    return this.args.disabled || false;
+  }
+
+  get maxlength() {
+    return this.args.maxlength || 30;
+  }
+}
