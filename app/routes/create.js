fe29f0974a3321c861454e0184821bdc2165e75d
Akshai Sarma <asarma@yahoo-inc.com>
2020-06-05 12:45:15 -0700

    Natives classes for all routes but result

--- app/routes/create.js
+++ app/routes/create.js
@@ -6,20 +6,20 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-  queryManager: service(),
+export default class CreateRoute extends Route {
+  @service queryManager;
 
   serialize(model) {
     return { hash: model.get('hash') };
-  },
+  }
 
   model(params) {
     let hash = params.hash;
     let manager = this.queryManager;
     return manager.decodeQuery(hash).then(object => manager.copyQuery(object));
-  },
+  }
 
   afterModel(model) {
     this.transitionTo('query', model.get('id'));
   }
-});
+}
