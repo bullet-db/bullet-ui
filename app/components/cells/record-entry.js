5159aee8d0eaf2caa6e2a189ce3dd35baef0656c
Akshai Sarma <asarma@yahoo-inc.com>
2020-07-21 15:49:36 -0700

    Migrating record-entry

--- app/components/cells/record-entry.js
+++ app/components/cells/record-entry.js
@@ -3,65 +3,67 @@
  *  Licensed under the terms of the Apache License, Version 2.0.
  *  See the LICENSE file associated with the project for terms.
  */
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { next } from '@ember/runloop';
 import { isEmpty, typeOf } from '@ember/utils';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
-import ElementPopover from 'bullet-ui/mixins/element-popover';
 
-export default Component.extend(ElementPopover, {
-  classNames: ['record-entry'],
-  classNameBindings: ['hasPopover:has-popover'],
-  row: null,
-  column: null,
-  value: null,
-  isActive: false,
-  // ElementPopover properties
-  titleElement: '.record-popover-title',
-  bodyElement: '.record-popover-body',
-  placeOn: 'bottom',
-  additionalClass: 'record-entry-popover',
+export default class RecordEntryComponent extends Component {
+  @tracked hasPopover;
+  @tracked showingPopover;
 
-  data: computed('row', 'column', function() {
-    let columnName = this.get('column.label');
+  constructor() {
+    super(...arguments);
+    this.hasPopover = !isEmpty(this.data);
+    this.showingPopover = false;
+  }
+
+  willDestroy() {
+    this.showingPopover = false;
+  }
+
+  get popperOptions() {
+    return {
+      modifiers: {
+        preventOverflow: {
+          escapeWithReference: false
+        }
+      }
+    };
+  }
+
+  get label() {
+    return this.args.column.get('label');
+  }
+
+  get data() {
     // Deliberately not using Ember get to prevent '.' expansion
-    return this.get('row.content')[columnName];
-  }).readOnly(),
+    return this.args.row.get('content')[this.label];
+  }
 
-  hasPopover: computed('data', function() {
-    return !isEmpty(this.data);
-  }).readOnly(),
-
-  isComplex: computed('data', function() {
+  get isComplex() {
     let type = typeOf(this.data);
     return type === 'object' || type === 'array';
-  }).readOnly(),
+  }
 
-  textValue: computed('isComplex', 'data', function() {
+  get textValue() {
     let data = this.data;
     return this.isComplex ? JSON.stringify(data) : data;
-  }).readOnly(),
+  }
 
-  formattedValue: computed('isComplex', 'data', function() {
+  get formattedValue() {
     let data = this.data;
     return this.isComplex ? JSON.stringify(data, null, 2) : data;
-  }).readOnly(),
-
-  willDestroyElement() {
-    this._super(...arguments);
-    this.removePopover();
-  },
-
-  click() {
-    this.set('isActive', true);
-    next(() => {
-      this.getPopover().popover('toggle');
-    });
-  },
-
-  actions: {
-    closePopover() {
-      this.getPopover().popover('hide');
-    }
   }
-});
+
+  @action
+  closePopover() {
+    this.showingPopover = false;
+  }
+
+  @action
+  showPopover() {
+    this.showingPopover = true;
+  }
+}
