fc48d7186a574baadff848b13f753900767c47bf
Akshai Sarma <asarma@yahoo-inc.com>
2020-08-17 13:53:50 -0700

    Lint fix

--- tests/unit/instance-initializers/migrations-test.js
+++ tests/unit/instance-initializers/migrations-test.js
@@ -3,7 +3,6 @@
  *  Licensed under the terms of the Apache License, Version 2.0.
  *  See the LICENSE file associated with the project for terms.
  */
-import { isEmpty } from '@ember/utils';
 import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import Application from 'bullet-ui/app';
