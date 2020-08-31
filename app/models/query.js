15b2f1b3aaf57431b9202114d07a8f8837e6b5a6
Akshai Sarma <asarma@yahoo-inc.com>
2020-08-31 10:56:38 -0700

    Typo

--- app/models/query.js
+++ app/models/query.js
@@ -1,4 +1,4 @@
-AGGREGATION_TYPES.describe(AGGREGATION_TYPES.RAW)/*
+/*
  *  Copyright 2016, Yahoo Inc.
  *  Licensed under the terms of the Apache License, Version 2.0.
  *  See the LICENSE file associated with the project for terms.
